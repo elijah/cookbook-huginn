e3952fb924b410af77b06e080dd4b153b5d21469
Elijah Wright <ewright@cloudpassage.com>
2015-06-28 14:59:58 -0500

    Initial commit.  Pulling huginn/deployment cookbook files (from the huginn_production site cookbook, which works) into their own cookbook, for further improvement.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'huginn'
+maintainer       'Elijah Wright'
+maintainer_email 'elijah.wright@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures huginn'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
