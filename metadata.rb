e7e026d1e7cce35c3374bb13ce7c5a1d66e83289
Elijah Wright <ewright@cloudpassage.com>
2015-07-21 14:15:43 -0500

    Add dependency on database cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'elijah.wright@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures huginn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 depends          'mysql'
+depends          'database'
 depends          'mysql2_chef_gem', '~> 1.0.1'
 
